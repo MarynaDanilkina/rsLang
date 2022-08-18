5120b16d2f1bf3159ca6018ff68fd031bdbc2210
Pavel Kizhlo <pavelkizh@gmail.com>
2022-08-18 16:00:57 +0300

    feat: add 'Word' class for interaction with api

---
+++ src/api/words.ts
@@ -0,0 +1,24 @@
+import { WordData } from '../interfaces/interfaces';
+
+class Words {
+    private baseURL: string;
+
+    private words: string;
+
+    constructor() {
+        this.baseURL = 'https://rs-lang-kdz.herokuapp.com';
+        this.words = `${this.baseURL}/words`;
+    }
+
+    async getWords(groupNumber: number, pageInGroup: number) {
+        const response = await fetch(`${this.words}?group=${groupNumber}&page=${pageInGroup}`);
+        return (await response.json()) as Array<WordData>;
+    }
+
+    async getWord(wordId: string) {
+        const response = await fetch(`${this.words}/${wordId}`);
+        return (await response.json()) as WordData;
+    }
+}
+
+export default Words;
