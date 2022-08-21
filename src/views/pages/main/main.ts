c69465b4ceb74743a545bb3bbb0566b1e2ea9867
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-08-21 21:14:36 +0300

    feat: add video section styles

--- src/views/pages/main/main.ts
+++ src/views/pages/main/main.ts
@@ -4,6 +4,7 @@ import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import GreetingSection from '../../components/mainContent/greetingSection/greetingSection';
 import TitleSection from '../../components/mainContent/titleSection/titleSection';
+import VideoSection from '../../components/mainContent/videoSection/videoSection';
 
 export default class Main implements View {
     html = `<main id="main"></main>`;
@@ -13,11 +14,13 @@ export default class Main implements View {
         const footer = new Footer();
         const tilteSection = new TitleSection();
         const greetingSection = new GreetingSection();
+        const videoSection = new VideoSection();
 
         header.render();
         htmlConsts.BODY.insertAdjacentHTML('beforeend', this.html);
         tilteSection.render();
         greetingSection.render();
+        videoSection.render();
         footer.render();
     }
 }
