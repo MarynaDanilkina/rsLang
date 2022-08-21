57824a3091a9e080261c19a1ebbb1e20aff2c0c5
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-08-21 21:20:58 +0300

    feat: add information section styles

--- src/views/pages/main/main.ts
+++ src/views/pages/main/main.ts
@@ -3,6 +3,7 @@ import htmlConsts from '../../../models/htmlElements';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import GreetingSection from '../../components/mainContent/greetingSection/greetingSection';
+import InformationSection from '../../components/mainContent/informationSection/informationSection';
 import TitleSection from '../../components/mainContent/titleSection/titleSection';
 import VideoSection from '../../components/mainContent/videoSection/videoSection';
 
@@ -15,12 +16,14 @@ export default class Main implements View {
         const tilteSection = new TitleSection();
         const greetingSection = new GreetingSection();
         const videoSection = new VideoSection();
+        const informationSection = new InformationSection();
 
         header.render();
         htmlConsts.BODY.insertAdjacentHTML('beforeend', this.html);
         tilteSection.render();
         greetingSection.render();
         videoSection.render();
+        informationSection.render();
         footer.render();
     }
 }
