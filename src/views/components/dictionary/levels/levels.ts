4cb46d9cfda398ef322fbcf181e579852055c938
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-08-22 18:17:21 +0300

    refactor: delete owl

--- src/views/components/dictionary/levels/levels.ts
+++ src/views/components/dictionary/levels/levels.ts
@@ -4,16 +4,7 @@ import './levels.sass';
 import levelsMap from './levelsMap';
 
 export default class Levels implements View {
-    html = `<div class="levels-wrapper" id="levels-wrapper">
-                <div class="owl__img">
-                    <div class="diagol">
-                        <h3>Добро пожаловать в учебник!</h3>
-                        <p>Для начало, выбери уровень уложности</p>
-                    </div>
-                    <img class="owl" src="../../../../assets/images/owl.png" alt="Owl">
-                </div>
-            </div>
-            <div class="levels__difficulty">
+    html = `<div class="levels__difficulty">
                 <h2 class="levels__title">Выберите уровень сложности:</h2>
                 <div class="levels__container" id="levels__container">
                 </div>
