f1f22d621cce0481d6f62c14fdbdb59253538efd
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-08-23 08:18:16 +0300

    feat: add statistic page

--- src/views/pages/statistic/statistic.ts
+++ src/views/pages/statistic/statistic.ts
@@ -3,6 +3,7 @@ import htmlElements from '../../../models/htmlElements';
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 import HeadingSection from '../../components/statistic/headingSection/headingSection';
+import StatisticAll from '../../components/statistic/statistilAll/statistic';
 
 export default class Statistic implements View {
     html = `<main id="main"></main>`;
@@ -11,10 +12,12 @@ export default class Statistic implements View {
         const header = new Header();
         const footer = new Footer();
         const headingSection = new HeadingSection();
+        const statisticAll = new StatisticAll();
 
         header.render();
         htmlElements.BODY.insertAdjacentHTML('beforeend', this.html);
         headingSection.render();
+        statisticAll.render();
         footer.render();
     }
 }
