516843a91c4005426376fb63afddac7cbb6a4a82
Pavel Kizhlo <pavelkizh@gmail.com>
2022-08-25 19:29:07 +0300

    feat: add utils.ts file with auxiliary functions

---
+++ src/utils/utils.ts
@@ -0,0 +1,59 @@
+export function testEmail(this: HTMLInputElement) {
+    this.classList.add('warning');
+    const emailRegExp = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const isValid = emailRegExp.test(this.value);
+
+    if (isValid || !this.value) {
+        this.classList.remove('warning');
+    }
+
+    if (isValid) {
+        this.classList.add('valide');
+    } else {
+        this.classList.remove('valide');
+    }
+}
+
+export function testLength(this: HTMLInputElement, length: number) {
+    this.classList.add('warning');
+    const isValid = this.value.length > length;
+
+    if (isValid || !this.value) {
+        this.classList.remove('warning');
+    }
+
+    if (isValid) {
+        this.classList.add('valide');
+    } else {
+        this.classList.remove('valide');
+    }
+}
+
+export function equalCheck(this: HTMLInputElement, input: HTMLInputElement) {
+    this.classList.add('warning');
+    const isValid = this.value === input.value;
+
+    if (isValid || !this.value) {
+        this.classList.remove('warning');
+    }
+
+    if (isValid) {
+        this.classList.add('valide');
+    } else {
+        this.classList.remove('valide');
+    }
+}
+
+export function enableSubmit(this: HTMLButtonElement, inputs: Array<HTMLInputElement>) {
+    const isValide = inputs.every((input) => input.classList.contains('valide'));
+    this.disabled = !isValide;
+}
+
+export function showSpinner(isShown: boolean) {
+    const spinner = <HTMLDivElement>document.querySelector('.lds-roller');
+    if (isShown) {
+        spinner.classList.add('active');
+    } else {
+        spinner.classList.remove('active');
+    }
+}
