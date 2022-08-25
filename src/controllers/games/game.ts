fa2fe1f6d9e8156884462d99de626dc2ee795d36
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-08-25 13:06:35 +0300

    feat: create common class for Games

--- src/controllers/games/game.ts
+++ src/controllers/games/game.ts
@@ -1,7 +1,16 @@
+import Words from '../../api/words';
+import { WordData } from '../../interfaces/interfaces';
 import Levels from '../../views/components/dictionary/levels/levels';
 import levelsMap from '../../views/components/dictionary/levels/levelsMap';
+import Audiocall from '../../views/pages/games/audiocall/audiocall';
 
-export class Game {
+export default class Game {
+    gameLevel: number | undefined;
+
+    words: WordData[] | undefined;
+
+    gameType: 'Audiocall' | 'Sprint' | undefined;
+
     renderLevels() {
         const GAME_CONTAINER = <HTMLElement>document.getElementById('game__container');
         const levels = new Levels(levelsMap, GAME_CONTAINER);
@@ -10,9 +19,48 @@ export class Game {
         GAME_CONTAINER.innerHTML = '';
         levels.render();
         GAME_CONTAINER.insertAdjacentHTML('beforeend', playBtnHtml);
+
+        this.levelsGamePageListners();
     }
+
+    selectLevel(e: Event) {
+        const selectedBtn = <HTMLButtonElement>e.currentTarget;
+        const LEVELS_BTNS = Array.from(document.querySelectorAll('.button__level'));
+
+        LEVELS_BTNS.forEach((btn) => btn.classList.remove('selected'));
+        selectedBtn.classList.add('selected');
+        this.gameLevel = LEVELS_BTNS.indexOf(selectedBtn);
+    }
+
+    async startGame() {
+        const GAME_CONTAINER = <HTMLElement>document.getElementById('game__container');
+        const wordsAPI = new Words();
+
+        this.words = await wordsAPI.getWordsinGroup(<number>this.gameLevel);
+        GAME_CONTAINER.innerHTML = '';
+
+        if (this.gameType === 'Audiocall') {
+            const game = new Audiocall();
+            game.render();
+        }
+        if (this.gameType === 'Sprint') {
+            // const game = new Sprint();
+            // game.render()
+        }
+    }
+
     discriptionGamePageListners() {
         const PLAY_BTN = <HTMLDivElement>document.querySelector('.btn-play');
-        PLAY_BTN.addEventListener('click', this.renderLevels);
+
+        PLAY_BTN.addEventListener('click', this.renderLevels.bind(this));
+    }
+
+    levelsGamePageListners() {
+        const LEVELS_BTNS = document.querySelectorAll('.button__level');
+        const PLAY_BTN = <HTMLDivElement>document.querySelector('.btn-play');
+
+        LEVELS_BTNS.forEach((btn) => btn.addEventListener('click', (e: Event) => this.selectLevel(e)));
+
+        PLAY_BTN.addEventListener('click', this.startGame.bind(this));
     }
 }
