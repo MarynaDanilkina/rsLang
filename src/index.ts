a58684811048fe306b9c7866ec6abfa3e1cd071c
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-08-27 18:27:01 +0300

    feat: save local stste to local storage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import State from './models/state';
 import Router from './router/router';
 import './style.sass';
 
@@ -5,3 +6,10 @@ const router = new Router();
 
 router.routWindowHandler();
 router.routClickHandler();
+
+window.addEventListener('unload', () => {
+    window.localStorage.setItem(State.currentUserID, JSON.stringify(State));
+});
+if (window.localStorage.getItem('')) {
+    const storageState = JSON.parse(<string>window.localStorage.getItem(''));
+}
