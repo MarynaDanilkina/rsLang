cb3f4035908bacc9f1b3a98cc2611c7205fd04d5
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-09-04 22:07:49 +0300

    feat: add video

--- src/views/components/mainContent/videoSection/videoSection.ts
+++ src/views/components/mainContent/videoSection/videoSection.ts
@@ -5,8 +5,9 @@ export default class VideoSection implements View {
     html = `
     <section id="videoSection" class="video_section">
       <div class="player">
-        <img class="playerImg" src="../../../assets/images/player.png" /> 
-        <img class="playVideo" src="../../../assets/images/playVideo.svg" /> 
+        <iframe width="50%" height="500px"
+            src="https://www.youtube.com/embed/tgbNymZ7vqY">
+        </iframe>
       </div>
     </section>`;
 
