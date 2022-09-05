b9b20c8ed5e09c51bdffb33bb2870d1e4f597e89
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-09-06 00:04:33 +0300

    feat: add gh links

--- src/views/components/team/team.ts
+++ src/views/components/team/team.ts
@@ -6,27 +6,27 @@ export default class Team implements View {
               <h3 class="team__header">Над проектом работали</h3>
               <div class="teams">
                 <div class="team">
-                  <div class="team__member">
+                  <a href="https://github.com/PavelKizhlo" target="_blank" class="team__member">
                     <div class="team__img-bg">
                       <div id="Pavel" class="team__img"></div>
                     </div>
                     <p class="team__name">Pavel Kizhlo</p>
                     <p class="team__talent">Авторизация, игра 'Спринт', Backend</p>
-                  </div>
-                  <div class="team__member">
+                  </a>
+                  <a href="https://github.com/MarynaDanilkina" target="_blank" class="team__member">
                     <div class="team__img-bg">
                       <div id="Maryna" class="team__img"></div>
                     </div>
                     <p class="team__name">Maryna Danilkina</p>
                     <p class="team__talent">Электронный учебник, карточка слова, дизайн</p>
-                  </div>
-                  <div class="team__member">
+                  </a>
+                  <a href="https://github.com/vzakharenkova" target="_blank" class="team__member">
                     <div class="team__img-bg">
                       <div id="Victoria" class="team__img"></div>
                     </div>
                     <p class="team__name">Victoria Zakharenkova</p>
                     <p class="team__talent">Главная страница, игра 'Аудиовызов', статистика</p>
-                  </div>
+                  </a>
                 </div>
               </div>
             </div>`;
